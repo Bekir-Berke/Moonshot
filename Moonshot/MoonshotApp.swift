360e00d6e8b6d7fc064eee573b07c55dc9f8512a
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-11-11 12:43:01 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Bekir Berke Yılmaz on 11.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
